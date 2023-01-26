81ec9de000745dd44028a8bd98e34f1bde355852
joySutradhor <joysutradhorcmt@gmail.com>
2023-01-26 19:41:14 +0600

    solution 4 js problem

---
+++ first.js
@@ -0,0 +1,37 @@
+
+// solution one 
+
+let capital = 1000 ;
+let apple = 350 ;
+let orange = 350 ;
+let totalCost = apple + orange ;
+let result = capital - totalCost ;
+// console.log(result);
+
+// solution two 
+
+let Mathematics = 75.25 ;
+let Biology = 65 ;
+let Chemistry = 80;
+let Physics = 35.45;
+let Bangla = 99.50;
+
+let total = Mathematics + Biology + Chemistry + Physics +Bangla ;
+let avg = total / 5 ;
+let finalResult = avg.toFixed(2);
+// console.log(finalResult);
+
+// solution Three 
+
+let fline = "I am going to be " ;
+let lline = "an awesome web developer" ;
+let fullSentence = fline + lline ;
+
+// console.log(fullSentence);
+
+// solution four 
+
+let number1 = 119 ;
+let number2 = 5 ;
+let reminder = 119 % 5 ;
+console.log (reminder) ;
